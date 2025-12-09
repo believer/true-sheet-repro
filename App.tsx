fb047accff37dace81d7ab6e11bb812a11251f82
Natt och Dag, Rickard <rickard.natt-och-dag@seb.se>
2025-12-09 09:53:00 +0100

    chore: add repro for background color bug

--- App.tsx
+++ App.tsx
@@ -1,45 +1,25 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import { TrueSheet } from "@lodev09/react-native-true-sheet";
+import { Button, Text, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
+	return (
+		<SafeAreaProvider>
+			<SafeAreaView>
+				<Button
+					onPress={async () => {
+						await TrueSheet.present("sheet");
+					}}
+					title="Open sheet"
+				/>
+				<View>
+					<TrueSheet name="sheet" backgroundColor="red">
+						<Text>HELLO</Text>
+					</TrueSheet>
+				</View>
+			</SafeAreaView>
+		</SafeAreaProvider>
+	);
 }
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
